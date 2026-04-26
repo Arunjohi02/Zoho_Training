e14be9e915f46144d49c8429ab0608b11325925b
Arunjohi <arunjohi2004@gmail.com>
2026-04-26 14:20:24 +0530

    create line class for put line

---
+++ src/Clint/Line.java
@@ -0,0 +1,10 @@
+package Clint;
+
+public class Line {
+	void Line() {
+		System.out.println("-------------------------------------------------------");
+	}
+	void dataline(){
+		System.out.println("-------------------------------");
+	}
+}
